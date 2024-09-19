7efdc2c26deadcf3ece45a67675fa2269ecbfd8a
kubaskubas <donieltypedale333@gmail.com>
2024-09-19 14:59:58 +0200

    numer 2

--- lekja3-zad2/main.cpp
+++ lekja3-zad2/main.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-
+#include <fstream>
 using namespace std;
+
+vector<string> d_weX;
+vector<string> d_weY;
+vector<int> tab;
+
+struct punkt{
+    string x, y;
+};
+
+vector<punkt> pkt;
 
 int main()
 {
-    ifstream plik("s³owa-txt.txt");
-    vector<string> wd;
-    vector<vector<int>> wd2;
 
-    while (!plik.eof())
+    vector<vector<int>> liczby;
+
+    vector<int> linia;
+
+    liczby.push_back(linia);
+
+
+        ifstream we("liczby2txt.txt");
+
+    punkt p[100];
+    int i = 0;
+    while (!we.eof())
     {
+        we >> p[i].x >> p[i].y;
+        d_weX.push_back(p[i].x);
+        d_weY.push_back(p[i].y);
+        pkt.push_back({p[i].x, p[i].y});
+        i++;
+    }
 
+    we.close();
+
+    for(auto& item : pkt){
+        cout << item.x << endl << item.y << endl;
     }
 
     return 0;
